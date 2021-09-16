5f9c8551f020b4982978f51d4d2d1254dd653644
Adina Renner <adina.renner@gmail.com>
2021-09-16 16:13:23 +0200

    added chevrons

--- app/controllers/recommendations.js
+++ app/controllers/recommendations.js
@@ -1,8 +1,11 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { inject as controller } from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 
 export default class extends Controller {
   @service data;
   @controller application;
+
+  @tracked state = {};
 }
