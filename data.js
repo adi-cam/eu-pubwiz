7111762c734ffca85c1284e88539ddef68b6e254
Adina Renner <adina.renner@gmail.com>
2021-09-16 13:22:58 +0200

    collect data in directory

--- data.js
+++ data.js
@@ -5,19 +5,21 @@ const csvParse = require('csv-parse/lib/sync');
 
 class FeedBuilder extends Plugin {
   build() {
-    // read questions
-    const rawQuestions = this.input.readFileSync('./questions.csv');
+    this.input.readdirSync('.').forEach((path) => {
+      // read file
+      const file = this.input.readFileSync(path);
 
-    // parse questions
-    const questions = csvParse(rawQuestions, {
-      columns: true,
+      // parse csv
+      const data = csvParse(file, {
+        columns: true,
+      });
+
+      // write json
+      this.output.writeFileSync(path.replace('.csv', '.json'), JSON.stringify(data));
     });
-
-    // write file
-    this.output.writeFileSync(`questions.json`, JSON.stringify(questions));
   }
 }
 
 module.exports = function () {
-  return new FeedBuilder([new WatchedDir('')]);
+  return new FeedBuilder([new WatchedDir('data')]);
 };
