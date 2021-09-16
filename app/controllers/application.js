0c19df95be71b3a455b99ca03ad1d2e27653ad3d
Adina Renner <adina.renner@gmail.com>
2021-09-16 15:34:31 +0200

    handle icon buttons

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,18 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class extends Controller {
   queryParams = ['title', 'answers'];
 
   @tracked title = '';
   @tracked answers = {};
+
+  @action print() {
+    window.print();
+  }
+
+  @action share() {
+    window.location = `mailto:xyz@abc.com?subject=EU%20Publication%20Wizard&body=Visit%20here:%20${window.location}`;
+  }
 }
